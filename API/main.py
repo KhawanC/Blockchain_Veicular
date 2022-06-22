a0a2f41e48d0d081decc469f8b0a9d14b4d5bb92
KhawanC <kauacassiano121@gmail.com>
2022-06-22 08:01:14 -0300

    removendo lixo

--- API/main.py
+++
@@ -1,6 +0,0 @@
-from src.server.instance import server
-from src.controllers.veiculos import *
-
-
-if __name__ == '__main__':
-    app.run(debug=True)
--- Front/scripts/menu_toggle.js
+++
@@ -1,8 +0,0 @@
-let toggle = document.querySelector('.toggle');
-let nav = document.querySelector('.navigation');
-let main = document.querySelector('.main');
-
-toggle.onclick = function() {
-    nav.classList.toggle('ativo')
-    main.classList.toggle('ativo')
-}
