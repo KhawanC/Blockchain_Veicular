0402ef60f14202f6eba8545eb2c0f94f1c8fd7a8
KhawanC <kauacassiano121@gmail.com>
2022-06-22 15:04:21 -0300

    swapando requisições para receber um JSON, limpando chaincode para atender os dados OBD

--- API/requisicaoLedger.py
+++ API/requisicaoLedger.py
@@ -1,8 +1,8 @@
 from crypt import methods
 from hfc.fabric import Client as client_fabric
-import asyncio
 from flask import *
 from tornado.platform.asyncio import AnyThreadEventLoopPolicy
+import asyncio
 
 domain = "ptb.de"
 channel_name = "nmi-channel"
@@ -13,13 +13,42 @@ app = Flask(__name__)
 
 asyncio.set_event_loop_policy(AnyThreadEventLoopPolicy())
 
-@app.route('/inserir_teste/&=<hash>&=<vin>&=<co>')
-def hello_word(hash, vin, co):
-    return "teste {} {} {}".format(hash, vin, co)
+@app.route('/inserir_veiculo', methods=['POST'])
+def insertVeiculo(hash, vin, carbono):
+    
+    request_data = request.get_json()
+    
+    loop = asyncio.get_event_loop()
 
+    c_hlf = client_fabric(net_profile=(domain + ".json"))
 
-@app.route('/inserir_veiculo/&=<string:hash>&=<string:vin>&=<int:carbono>', methods=['POST'])
-def insertBlockchain(hash, vin, carbono):
+    admin = c_hlf.get_user(domain, 'Admin')
+    
+    callpeer = "peer0." + domain
+    
+    c_hlf.new_channel(channel_name)
+
+    response = loop.run_until_complete(
+        c_hlf.chaincode_invoke(requestor=admin,
+                               channel_name=channel_name,
+                               peers=[callpeer],                               
+                               args=[request_data["Vim"], request_data["Hash"], request_data["Co2"]],
+                               cc_name=cc_name,
+                               cc_version=cc_version,
+                               fcn='registrarVeiculo',
+                               cc_pattern=None))
+    
+    print(response)
+    
+    return {
+        "status": 200,
+        "mensagem": "Veiculo registrado com sucesso!"
+    }
+
+@app.route('/inserir_fabricante', methods=['POST'])
+def insertFabricante():
+    
+    request_data = request.get_json()
 
     loop = asyncio.get_event_loop()
 
@@ -35,18 +64,19 @@ def insertBlockchain(hash, vin, carbono):
         c_hlf.chaincode_invoke(requestor=admin,
                                channel_name=channel_name,
                                peers=[callpeer],                               
-                               args=[vin, hash, str(carbono)],
+                               args=[request_data["nome"]],
                                cc_name=cc_name,
                                cc_version=cc_version,
-                               fcn='registrarVeiculo',
+                               fcn='registrarFabricante',
                                cc_pattern=None))
     
     print(response)
     
     return {
         "status": 200,
-        "mensagem": "Veiculo registrado com sucesso!"
+        "mensagem": "Fabricante registrado com sucesso!"
     }
+    
 
 if __name__ == "__main__":
     app.run(debug=True, port=8001, host="0.0.0.0")
--- Chaincode/fabpki.go
+++ Chaincode/fabpki.go
@@ -59,23 +59,9 @@ type ModeloVeiculo struct { //"model-"
 }
 
 type Veiculo struct { //"user-"
-	IdCdgModelo         string `json:"IdCdgModelo"` //FK (Categoria)
-	AcumuladorDistancia string `json:"AcumuladorDistancia"`
-}
-
-type Veiculo2 struct { //"user-"
 	Hash string `json:"Hash"`
 	Vim  string `json:"Vim"`
 	Co2  string `json:"Co2"`
-}
-
-type Trajeto struct { //"traj-"
-	TrajetoDistancia string `json:"TrajetoDistancia"`
-}
-
-type TrajetoVeiculo struct { //"traj_user-"
-	IdPlaca   string `json:"IdPlaca"`   //FK (Veiculo)
-	IdTrajeto string `json:"IdTrajeto"` //FK (Trajeto)
 }
 
 type Fabricante struct { //"fab-""
@@ -100,16 +86,10 @@ func (s *SmartContract) Init(stub shim.ChaincodeStubInterface) sc.Response {
 func (s *SmartContract) Invoke(stub shim.ChaincodeStubInterface) sc.Response {
 	fn, args := stub.GetFunctionAndParameters()
 
-	if fn == "registrarModelo" {
-		return s.registrarModelo(stub, args)
-	} else if fn == "registrarVeiculo" {
+	if fn == "registrarVeiculo" {
 		return s.registrarVeiculo(stub, args)
-	} else if fn == "registrarTrajeto" {
-		return s.registrarTrajeto(stub, args)
 	} else if fn == "registrarFabricante" {
 		return s.registrarFabricante(stub, args)
-	} else if fn == "registrarCarbono" {
-		return s.registrarCarbono(stub, args)
 	} else if fn == "registrarCredito" {
 		return s.registrarCredito(stub, args)
 	} else if fn == "anunciarOrdem" {
@@ -121,40 +101,6 @@ func (s *SmartContract) Invoke(stub shim.ChaincodeStubInterface) sc.Response {
 	}
 
 	return shim.Error("Chaincode não suporta essa função.")
-}
-
-//Função que recebe ./bancoLedger.py e inserer o banco de dados com os veículos
-func (s *SmartContract) registrarModelo(stub shim.ChaincodeStubInterface, args []string) sc.Response {
-
-	//Verificando se a quantidade de argumnetos é maior que 6
-	if len(args) != 6 {
-		return shim.Error("Não foi encontrado nenhum argumento. Tente novamente!")
-	}
-
-	//Inserindo argumentos dentro de variáveis
-	cdg := args[0]
-	categoria := args[1]
-	marca := args[2]
-	versao := args[3]
-	modelo := args[4]
-	emissao_co2 := args[5]
-
-	var categoriaInfor = ModeloVeiculo{ //Inserindo argumentos dentro da Struct Categoria
-		Categoria:  categoria,
-		Marca:      marca,
-		Versao:     versao,
-		Modelo:     modelo,
-		EmissaoCo2: emissao_co2,
-	}
-
-	categoriaAsBytes, _ := json.Marshal(categoriaInfor) //Encapsulando as informações em arquivo JSON
-
-	idCdgLedger := "model-" + cdg
-
-	stub.PutState(idCdgLedger, categoriaAsBytes) //Inserindo valores no ledger, com uma informação associada à uma chave
-
-	fmt.Println("Sucesso ao registrar  modelo de veiculo")
-	return shim.Success(nil)
 }
 
 func (s *SmartContract) registrarFabricante(stub shim.ChaincodeStubInterface, args []string) sc.Response {
@@ -195,7 +141,7 @@ func (s *SmartContract) registrarVeiculo(stub shim.ChaincodeStubInterface, args 
 	co2 := args[2]
 
 	//Criar Struct para manipular as informações do veículo
-	userVeiculo := Veiculo2{
+	userVeiculo := Veiculo{
 		Hash: hash,
 		Vim:  vim,
 		Co2:  co2,
@@ -207,146 +153,6 @@ func (s *SmartContract) registrarVeiculo(stub shim.ChaincodeStubInterface, args 
 	stub.PutState(idUserVeiculo, veiculoAsBytes)
 
 	fmt.Println("Sucesso ao registrar veiculo")
-	return shim.Success(nil)
-}
-
-func (s *SmartContract) registrarTrajeto(stub shim.ChaincodeStubInterface, args []string) sc.Response {
-
-	//Verificar se arquivo py retornou 2 argumentos
-	if len(args) != 2 {
-		return shim.Error("Eram esperados 2 argumentos... Tente novamente!")
-	}
-
-	//Dar nome aos argumentos
-	idPlaca := args[0]
-	userDistancia := args[1]
-
-	cdgUnicoTrajeto := Encode(AleatString(20))                  //Criando código unico para Struct trajeto
-	cdgUnicoUsuarioTrajeto := Encode(AleatString(20))           //Criando código unico para Struct trajeto
-	userDistFLoat, err := strconv.ParseFloat(userDistancia, 64) //Convertendo a distância recebida para float
-
-	//Recuperando dados do usuário
-	userAsBytes, err := stub.GetState(idPlaca)
-	if err != nil || userAsBytes == nil {
-		return shim.Error("Sua placa não existe.")
-	}
-
-	//Criando Struct para encapsular os dados
-	veiculo := Veiculo{}
-	json.Unmarshal(userAsBytes, &veiculo)
-
-	//Criar Struct do trajeto e do usuário
-	trajetoVeiculo := TrajetoVeiculo{}
-	trajeto := Trajeto{}
-
-	distAcumuladoString := veiculo.AcumuladorDistancia //Inserindo o acumulador de distância do usuário dentro de uma variável
-	distAcumuladoFloat, err := strconv.ParseFloat(distAcumuladoString, 64)
-
-	distAcumuladoFloat += userDistFLoat //Adicionar a distancia do trajeto feito ao acumulador
-	newDist := fmt.Sprintf("%g", distAcumuladoFloat)
-
-	veiculo.AcumuladorDistancia = newDist
-
-	//Criar assinatura do trajeto
-	trajeto.TrajetoDistancia = userDistancia
-
-	//Associar trajeto com o usuário
-	trajetoVeiculo.IdPlaca = idPlaca
-	trajetoVeiculo.IdTrajeto = "trajeto-" + cdgUnicoTrajeto
-
-	//Encapsulando dados em arquivo JSON
-	veiculoAsBytesFinal, _ := json.Marshal(veiculo)
-	trajetoAsBytes, _ := json.Marshal(trajeto)
-	veiculoTrajetoAsBytes, _ := json.Marshal(trajetoVeiculo)
-
-	//Inserindo valor no Ledger
-	idTrajeto := "trajeto-" + cdgUnicoTrajeto
-	idUsuarioTrajeto := "veic_traj-" + cdgUnicoUsuarioTrajeto
-
-	stub.PutState(idPlaca, veiculoAsBytesFinal)
-	stub.PutState(idTrajeto, trajetoAsBytes)
-	stub.PutState(idUsuarioTrajeto, veiculoTrajetoAsBytes)
-
-	fmt.Println("Sucesso ao registrar trajeto")
-	return shim.Success(nil)
-}
-
-func (s *SmartContract) registrarCarbono(stub shim.ChaincodeStubInterface, args []string) sc.Response {
-
-	//Verificar se arquivo py retornou 1 argumento
-	if len(args) != 1 {
-		return shim.Error("Era esperado 1 único argumento... Tente novamente!")
-	}
-
-	idPlaca := args[0]
-
-	//Recuperando dados do usuário
-	veiculoAsBytes, err := stub.GetState(idPlaca)
-	if err != nil || veiculoAsBytes == nil {
-		return shim.Error("Sua placa não existe.")
-	}
-
-	//Criando Struct para encapsular os dados do veiculo
-	veiculo := Veiculo{}
-	json.Unmarshal(veiculoAsBytes, &veiculo)
-
-	idModelo := veiculo.IdCdgModelo
-
-	//Recuperando dados do Modelo
-	modeloAsBytes, err := stub.GetState(idModelo)
-	if err != nil || modeloAsBytes == nil {
-		return shim.Error("Esse modelo não existe.")
-	}
-
-	//Criando Struct para encapsular os dados dp modelo
-	modelo := ModeloVeiculo{}
-	json.Unmarshal(modeloAsBytes, &modelo)
-
-	idFabricante := "fab-" + modelo.Marca
-
-	//Recuperando dados do Faricante
-	fabricanteAsBytes, err := stub.GetState(idFabricante)
-	if err != nil || fabricanteAsBytes == nil {
-		return shim.Error("Esse fabricante não existe.")
-	}
-
-	//Criando Struct para encapsular os dados dp modelo
-	fabricante := Fabricante{}
-	json.Unmarshal(fabricanteAsBytes, &fabricante)
-
-	disAcumuladorFloat, err := strconv.ParseFloat(veiculo.AcumuladorDistancia, 64)
-	modEmissCo2Float, err := strconv.ParseFloat(modelo.EmissaoCo2, 64)
-
-	co2Veiculo := disAcumuladorFloat * modEmissCo2Float
-	co2VeiculoArredondado := Arredondar(co2Veiculo)
-
-	fabCo2Float, err := strconv.ParseFloat(fabricante.Co2Tot, 64)
-
-	fabCo2Float += co2VeiculoArredondado
-	fabCO2String := fmt.Sprintf("%g", fabCo2Float)
-	fabricante.Co2Tot = fabCO2String
-	veiculo.AcumuladorDistancia = "0.0"
-
-	fmt.Println("-----registro de carbono---")
-	fmt.Println(veiculo)
-	fmt.Println(modelo)
-	fmt.Println(fabricante)
-
-	//Encapsulando dados em arquivo JSON
-	veiculoAsBytes, _ = json.Marshal(veiculo)
-	modeloAsBytes, _ = json.Marshal(modelo)
-	fabricanteAsBytes, _ = json.Marshal(fabricante)
-
-	fmt.Println("-----BYTES-----")
-	fmt.Println(veiculoAsBytes)
-	fmt.Println(modeloAsBytes)
-	fmt.Println(fabricanteAsBytes)
-
-	stub.PutState(idPlaca, veiculoAsBytes)
-	stub.PutState(idModelo, modeloAsBytes)
-	stub.PutState(idFabricante, fabricanteAsBytes)
-
-	fmt.Println("Sucesso ao computador co2 dos fabricantes")
 	return shim.Success(nil)
 }
 
