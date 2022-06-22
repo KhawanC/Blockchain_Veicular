83be50c5ee3da0beaf424967db82fe4b9a2fb84e
KhawanC <kauacassiano121@gmail.com>
2022-06-21 21:59:45 -0300

    organizando lógica para iniciar inserção de lances nas ordens

--- Chaincode/registrarLanceLedger.py
+++ Chaincode/registrarLanceLedger.py
@@ -12,11 +12,11 @@ listaFabricantes = []
 
 if __name__ == "__main__":
     
-    loop = asyncio.get_event_loop()
-    c_hlf = client_fabric(net_profile=(domain + ".json"))
-    admin = c_hlf.get_user(domain, 'Admin')
-    callpeer = "peer0." + domain
-    c_hlf.new_channel(channel_name)
+    # loop = asyncio.get_event_loop()
+    # c_hlf = client_fabric(net_profile=(domain + ".json"))
+    # admin = c_hlf.get_user(domain, 'Admin')
+    # callpeer = "peer0." + domain
+    # c_hlf.new_channel(channel_name)
     
     for doc in db.view('_all_docs'):
         i = doc['id']
@@ -27,11 +27,27 @@ if __name__ == "__main__":
         i = doc['id']
         if i[0:6] == "trans-":
             listaTransacoes.append(i)
-            
+    
+    print(listaTransacoes)
+    
+    idTransacao = int(input('Esses são as transações, qual você deseja? '))       
+    
+    compradorInfor = {}
+    
     for doc in db.find({
             "selector": {
-                "_id": "trans-KIA9f90dfc0713b8b7e3b22044487c9b091244720f9"
+                "_id": "{id}".format(id=listaTransacoes[idTransacao])
             }}):
         query_info = json.dumps(doc, indent=4, sort_keys=True)
         query_json = json.loads(query_info)
-        print(query_json)
+        transacaoInfor = query_json
+    
+    print(transacaoInfor)
+    print('Ultimo lance: ' + transacaoInfor['ValorLance'])
+    lance = input('Qual será o seu lance ? ')
+    
+    print(listaFabricantes)
+    idComprador = input('Quem é você? ')
+    
+    print('Dados: \n' + idComprador + '\n' + listaTransacoes[idTransacao] + '\n' + lance)
+        
