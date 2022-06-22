25d6c44b49cfc69d76f73c5ce5297be27213c052
KhawanC <kauacassiano121@gmail.com>
2022-06-22 16:16:44 -0300

    refatorando

--- API/requisicaoLedger.py
+++ API/requisicaoLedger.py
@@ -12,88 +12,87 @@ import couchdb, json
 
 app = Flask(__name__)
 
-asyncio.set_event_loop_policy(AnyThreadEventLoopPolicy())
+asyncio.set_event_loop_policy(AnyThreadEventLoopPolicy())    
 
-@app.route('/inserir_veiculo', methods=['POST'])
-def insertVeiculo():
-    
-    request_data = request.get_json()
-    
-    loop = asyncio.get_event_loop()
+@app.route('/inserir_fabricante', methods=['POST', 'GET'])
+def Fabricante():
+    if request.method == 'POST':
+        request_data = request.get_json()
 
-    c_hlf = client_fabric(net_profile=(domain + ".json"))
+        loop = asyncio.get_event_loop()
 
-    admin = c_hlf.get_user(domain, 'Admin')
-    
-    callpeer = "peer0." + domain
-    
-    c_hlf.new_channel(channel_name)
+        c_hlf = client_fabric(net_profile=(domain + ".json"))
 
-    response = loop.run_until_complete(
-        c_hlf.chaincode_invoke(requestor=admin,
-                               channel_name=channel_name,
-                               peers=[callpeer],                               
-                               args=[request_data["Vim"], request_data["Hash"], request_data["Co2"]],
-                               cc_name=cc_name,
-                               cc_version=cc_version,
-                               fcn='registrarVeiculo',
-                               cc_pattern=None))
-    return Response(response=json.dumps({
-        "status": 201,
-        "mensagem": "Veiculo registrado com sucesso"}), status=201, mimetype='application/json')
+        admin = c_hlf.get_user(domain, 'Admin')
+        
+        callpeer = "peer0." + domain
+        
+        c_hlf.new_channel(channel_name)
+        
+        fab_nome = request_data["nome"]
 
-@app.route('/inserir_fabricante', methods=['POST'])
-def insertFabricante():
+        response = loop.run_until_complete(
+            c_hlf.chaincode_invoke(requestor=admin,
+                                channel_name=channel_name,
+                                peers=[callpeer],                               
+                                args=[fab_nome.upper()],
+                                cc_name=cc_name,
+                                cc_version=cc_version,
+                                fcn='registrarFabricante',
+                                cc_pattern=None))
+        
+        return Response(response=json.dumps({
+            "status": 201,
+            "mensagem": "Fabricante registrado com sucesso"}), status=201, mimetype='application/json')
     
-    request_data = request.get_json()
+@app.route('/veiculo', methods=['GET', 'POST'])
+def Veiculo():
+    if request.method == 'GET':
+        listaVeiculos = []
+        server = couchdb.Server('http://localhost:5984/_utils')
+        couch = couchdb.Server()
+        db = couch['nmi-channel_fabpki']
+        
+        for doc in db.view('_all_docs'):
+                i = doc['id']
+                if i[0:5] == "veic-":
+                    for doc in db.find({
+                            "selector": {
+                            "_id": "{id}".format(id=i)
+                            }}):
+                            query_info = json.dumps(doc, indent=4, sort_keys=True)
+                            query_json = json.loads(query_info)
+                            infoVeiculo = query_json
+                            listaVeiculos.append(infoVeiculo)
+                    
+        return json.dumps(listaVeiculos), 200
+    
+    if request.method == 'POST':
+        request_data = request.get_json()
+        
+        loop = asyncio.get_event_loop()
 
-    loop = asyncio.get_event_loop()
+        c_hlf = client_fabric(net_profile=(domain + ".json"))
 
-    c_hlf = client_fabric(net_profile=(domain + ".json"))
+        admin = c_hlf.get_user(domain, 'Admin')
+        
+        callpeer = "peer0." + domain
+        
+        c_hlf.new_channel(channel_name)
 
-    admin = c_hlf.get_user(domain, 'Admin')
-    
-    callpeer = "peer0." + domain
-    
-    c_hlf.new_channel(channel_name)
-    
-    fab_nome = request_data["nome"]
+        response = loop.run_until_complete(
+            c_hlf.chaincode_invoke(requestor=admin,
+                                channel_name=channel_name,
+                                peers=[callpeer],                               
+                                args=[request_data["Vim"], request_data["Hash"], request_data["Co2"]],
+                                cc_name=cc_name,
+                                cc_version=cc_version,
+                                fcn='registrarVeiculo',
+                                cc_pattern=None))
+        return Response(response=json.dumps({
+            "status": 201,
+            "mensagem": "Veiculo registrado com sucesso"}), status=201, mimetype='application/json')
 
-    response = loop.run_until_complete(
-        c_hlf.chaincode_invoke(requestor=admin,
-                               channel_name=channel_name,
-                               peers=[callpeer],                               
-                               args=[fab_nome.upper()],
-                               cc_name=cc_name,
-                               cc_version=cc_version,
-                               fcn='registrarFabricante',
-                               cc_pattern=None))
-    
-    return Response(response=json.dumps({
-        "status": 201,
-        "mensagem": "Fabricante registrado com sucesso"}), status=201, mimetype='application/json')
-    
-@app.route('/listar_veiculos', methods=['GET'])
-def getVeiculo():
-    listaVeiculos = []
-    server = couchdb.Server('http://localhost:5984/_utils')
-    couch = couchdb.Server()
-    db = couch['nmi-channel_fabpki']
-      
-    for doc in db.view('_all_docs'):
-            i = doc['id']
-            if i[0:5] == "veic-":
-                  for doc in db.find({
-                        "selector": {
-                        "_id": "{id}".format(id=i)
-                        }}):
-                        query_info = json.dumps(doc, indent=4, sort_keys=True)
-                        query_json = json.loads(query_info)
-                        infoVeiculo = query_json
-                        listaVeiculos.append(infoVeiculo)
-                  
-    return json.dumps(listaVeiculos), 200
-    
 if __name__ == "__main__":
     app.run(debug=True, port=8001, host="0.0.0.0")
 
